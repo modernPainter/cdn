725a0055ac329cb0d7bf5260bd053233d88f0d43
Xinghai.Liu <xinghai.liu@ibaiqiu.com>
2023-09-22 14:46:09 +0800

    feat: 添加utils

--- web/utils/comUse.js
+++ web/utils/comUse.js
@@ -1787,8 +1787,27 @@ function getParams(name) {
 function getInterfaceURL(uri) {
 	return window.location.protocol + '//' + window.location.hostname + '/' + uri;
 }
+// 封装异步加载资源的方法
+function loadExternalResource(url, type) {
+	return new Promise((resolve, reject) => {
+		let tag;
 
-
+		if (type === "css") {
+			tag = document.createElement("link");
+			tag.rel = "stylesheet";
+			tag.href = url;
+		}
+		else if (type === "js") {
+			tag = document.createElement("script");
+			tag.src = url;
+		}
+		if (tag) {
+			tag.onload = () => resolve(url);
+			tag.onerror = () => reject(url);
+			document.head.appendChild(tag);
+		}
+	});
+}
 
 
 /*
